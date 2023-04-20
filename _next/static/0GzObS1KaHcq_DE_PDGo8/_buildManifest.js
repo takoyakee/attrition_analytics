cb2e9b116f4bbaec08c7267c70f6e08ff4638015
takoyakee <takoyakee@giithub.com>
2023-04-20 20:35:55 +0800

    Updates

---
+++ _next/static/0GzObS1KaHcq_DE_PDGo8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/134-5a2faa39569c8738.js","static/chunks/pages/index-d473ef91c5e179de.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
