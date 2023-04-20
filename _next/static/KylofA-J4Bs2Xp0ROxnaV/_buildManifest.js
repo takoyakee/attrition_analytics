ae89487121fcc8740254955cf2cbae5066a6a98f
takoyakee <takoyakee@giithub.com>
2023-04-20 20:51:00 +0800

    Updates

---
+++ _next/static/KylofA-J4Bs2Xp0ROxnaV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/134-5a2faa39569c8738.js","static/chunks/pages/index-449a5491226e6a1f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
